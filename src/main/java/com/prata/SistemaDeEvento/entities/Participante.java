51e2796366e8ce679f751fd883cff6e1bc099915
GabrielPraata <ghprata7245@gmail.com>
2025-05-24 10:35:14 -0300

    Participaante e configuracao H2

---
+++ src/main/java/com/prata/SistemaDeEvento/entities/Participante.java
@@ -0,0 +1,19 @@
+package com.prata.SistemaDeEvento.entities;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+   private String nome;
+   private String email;
+
+
+
+
+}
