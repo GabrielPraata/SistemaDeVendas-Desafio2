df4ba9e439a5b4cd18a5a29c4349aede5a01ff82
GabrielPraata <ghprata7245@gmail.com>
2025-05-24 09:24:55 -0300

    first commit

---
+++ src/test/java/com/prata/SistemaDeEvento/SistemaDeEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prata.SistemaDeEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
